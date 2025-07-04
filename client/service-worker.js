cdcd04fe84caa970ab7f60b299195ac9550d5fbc
richardgmsz <richardgmsdesouza@gmail.com>
2025-07-04 06:04:17 -0300

    Update service worker cache version

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,9 +1,21 @@
-const CACHE_NAME = 'santadicas-cache-v1';
+const CACHE_NAME = 'santadicas-cache-v2';
 const urlsToCache = ['/', '/app.js', '/style.css'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    )
   );
 });
 
